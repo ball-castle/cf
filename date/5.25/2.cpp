cf216aa5e7fd663cee2c2e455ec9b10576c03c0f
ball-castle <bin33138@gmail.com>
2026-05-25 22:58:26 +0800

    1

--- date/5.25/2.cpp
+++ date/5.25/2.cpp
@@ -3,17 +3,40 @@ using namespace std;
 using ll = long long;
 
 /*
-https://codeforces.com/problemset/problem/946/C
+https://codeforces.com/contest/2038/problem/B
 !!!!! 这题的思路在于操作顺序不重要 对于每个位置看次数就行
 !然后经典思路先变成0
-方程问题
-
+可以不变成0 如果最大值已经是2了，还没有达到那就是不行 因为这个时候操作不可能再让它达到
+然后从合法解可以回退
 */
 
 
 void solve() {
     ll n;
-    cin >       
+    cin >> n;
+    vector<ll> a(n + 1), ct(n + 1), c(n + 1);
+    ll mx = -4e18;
+    for(int i = 1; i <= n; i++) cin >> a[i];
+    
+    while(1) { // 这种循环的写法还蛮好玩的
+        mx = -4e18;
+        for(int i = 1; i <= n; i++) mx = max(mx, a[i]);
+        if(mx < 3) break;
+        for(int i = 1; i <= n; i++) {
+            c[i] = a[i] / 2;
+            ct[i] += c[i];
+        }
+        for(int i = 1; i <= n; i++) {
+            a[i] -= c[i] * 2;
+            a[i % n + 1] += c[i];
+        }
+    }
+
+    for(int i = 2; i <= n; i++) if(a[i] != a[1]) {cout << -1 << '\n'; return;}
+    ll mi = 4e18;
+    ll ans = 0;
+    for(int i = 1; i <= n; i++) mi = min(mi, ct[i]), ans += ct[i];
+    cout << ans - mi * n << '\n';
 }
 
 
@@ -28,7 +51,7 @@ int main() {
     cin.tie(0);
 
     int T;
-    T = 1;
+    cin >> T;
     while(T--) solve();
     return 0;
 }
