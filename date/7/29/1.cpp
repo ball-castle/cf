a933624307d58b963aeedb607a269a8a98940f02
ball-castle <bin33138@gmail.com>
2026-07-29 01:36:32 +0800

    1

---
+++ date/7/29/1.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+/*
+转化为就是求b有多少个因子
+学习一个sqrtl
+根号求因子个数
+*/
+
+void solve() {
+    ll cnt = 0;
+    ll b;
+    cin >> b;
+    for(ll i = 1; i <= b / i; i++) {
+        if(b % i == 0) {
+            cnt += 2;
+        }
+    }
+    ll r = sqrtl(b);
+    if(r * r == b) {
+        cnt--;
+    }
+    cout << cnt << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    ll T = 1;
+    // cin >> T;
+    while(T--) solve();
+    return 0;
+}
--- date/7/7.28/1.cpp
+++ date/7/7.28/1.cpp
@@ -4,13 +4,34 @@ using ll = long long;
 
 /*
 路径的本质其实是点的排列
-
+考虑排列的个数就行 组合问题
 */
+
+ll gcd(ll a, ll b) {
+    return b ? gcd(b, a % b) : a;
+}
 
 void solve() {
     ll n;
     cin >> n;
-    cout << n << '\n'; 
+
+    ll sum = 0;
+    vector<ll> a(n);
+    for(ll &x : a) cin >> x, sum += x;
+
+    sort(a.begin(), a.end());
+
+    ll sum2 = 0;
+    for(ll i = 0; i < n; i++) {
+        sum2 += a[i] * (2 * i - n + 1);
+    }
+
+    ll up = sum + 2 * sum2;
+    ll down = n;
+
+    ll g = gcd(up, down);
+    cout << up / g << ' ' << down / g << '\n';
+
 }
 
 int main() {
@@ -18,7 +39,7 @@ int main() {
     cin.tie(0);
 
     ll T = 1;
-    cin >> T;
+    // cin >> T;
     while(T--) solve();
     return 0;
 }
