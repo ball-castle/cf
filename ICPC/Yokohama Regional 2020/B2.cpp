95c51c6082a4532a4a9df4385dcea16d2faab17b
ball-castle <bin33138@gmail.com>
2026-08-26 20:06:32 +0800

    8.26

--- ICPC/Yokohama Regional 2020/B2.cpp
+++ ICPC/Yokohama Regional 2020/B2.cpp
@@ -4,70 +4,81 @@ using ll = long long;
 
 int main(){
 
-    int n,m;
-    cin>>n>>m;
-    vector<int>a(n+1),ap;
-    vector<int>b(n+1),bp;
-    a[0] = b[0] = 0;
-    for(int i=1;i<=n;i++){
-        cin>>a[i];
-        if(a[i]!=0){
-            ap.push_back(a[i]);
+    int n, m;
+    cin >> n>> m;
+
+    int N = n + m;
+
+    vector<int> a(n+1), b(m+1);
+
+    // need[x] : 1~x 中有多少个数属于 A
+    // type[x] : 1 -> x属于A
+    //           2 -> x属于B
+    vector<int>need(N + 1, -1);
+    vector<int>type(N + 1, 0);
+
+    for(int i = 1;i <= n; i++){
+        cin >> a[i];
+        if(a[i]){
+            need[a[i]] = i;
+            type[a[i]] = 1;
         }
     }
+
     for(int i=1;i<=m;i++){
         cin>>b[i];
-        if(b[i]!=0){
-            bp.push_back(b[i]);
-        }
-    }
-    ap.push_back(INT_MAX);
-    bp.push_back(INT_MAX);
-    int idxa=1,idxpa=0,idxb=1,idxpb=0;
-    //is filling  -- tail  
-    int now = 1;
-    while(now != m+n+1){
-        //cout<<"now is"<<now<<"\n";///////////////////////
-        if((idxb==m+1) || (b[idxb]&&b[idxb]!=now)||(idxa<=n && ap[idxpa] <= bp[idxpb] && now >= a[idxa-1] && now <= ap[idxpa] &&  (a[idxa]==0 || (a[idxa]!=0 && a[idxa]==now)))){
-            //cout<<"to a.";///////////////////////
-            if(a[idxa]==now){
-                idxpa++;
-                //cout<<"up";////////////////////////
-            }
-            a[idxa] = now;
-            idxa++;
-            now++;
-            //cout<<"\n\n";/////////////////////////////
-        }else{
-            //cout<<"to b.";///////////////////////
-            if(b[idxb]==now){
-                idxpb++;
-                //cout<<"up";////////////////////////
-            }
-            b[idxb] = now;
-            idxb++;
-            now++;
-            //cout<<"\n\n";/////////////////////////////////////////
+        if(b[i]){
+            need[b[i]]=b[i]-i;
+            type[b[i]]=2;
         }
     }
 
-    for(int i=1;i<=n;i++){
-        cout<<a[i]<<" ";
+    need[0]=0;
+
+    // 最终 1~N 中一定有 n 个属于 A
+    need[N] = n;
+
+    vector<int>belong(N + 1,0);
+    // belong[x] = 1 表示 x 属于 A
+    // 否则属于 B
+
+    int last=0;
+
+    for(int x = 1; x <= N; x++) {
+        if(need[x] == -1) continue;
+        int cnt = need[x] - need[last];
+        if(type[x] == 1) {
+            cnt--;
+            belong[x] = 1;
+        }
+        int r = x;
+        if(type[x]) r = x - 1;
+        for(int i = last + 1; i <= r && cnt; i++) {
+            belong[i] = 1;
+            cnt--;
+        }
+        last = x;
+    }
+    vector<int>ansA,ansB;
+
+    for(int x=1;x<=N;x++){
+        if(belong[x]){
+            ansA.push_back(x);
+        }
+        else{
+            ansB.push_back(x);
+        }
+    }
+
+    for(auto x:ansA){
+        cout<<x<<" ";
     }
     cout<<"\n";
-    for(int j=1;j<=m;j++){
-        cout<<b[j]<<" ";
+
+    for(auto x:ansB){
+        cout<<x<<" ";
     }
     cout<<"\n";
+
     return 0;
 }
-
-/*
-
-
-6 7
-0 5 0 0 0 0
-0 0 3 0 8 0 12
-
-
-*/
